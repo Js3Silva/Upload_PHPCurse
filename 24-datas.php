bdf963b9fed4621e7048a29ef7d93a0cb914b07c
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-01-22 15:13:19 -0300

    explanation dates

--- 24-datas.php
+++ 24-datas.php
@@ -1,1 +1,24 @@
 <?php
+// Datas
+date_default_timezone_set('America/Sao_Paulo');
+echo date('d/m/Y H:i:s');
+echo "<hr>";
+
+$date = date('Y-m-d'); //DATE
+$datetime = date('Y-m-d H:i:s'); //DATETIME
+
+// TIME
+$time = time();
+echo date('d/m/Y', $time);
+echo "<hr>";
+
+//MKTIME
+$data_pagamento = mktime(15, 30, 00, 02, 30, 2025);
+echo date('d/m - h:i A', $data_pagamento);
+echo "<hr>";
+
+// STRTOTIME
+$data = '2019-04-10 13:30:00';  // Quando resgatar do banco de dados
+$data = strtotime($data);
+
+echo date('d/m/Y', $data);
