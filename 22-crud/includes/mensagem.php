ac73e946aa21461dbd1dd72858a3e8dba832b377
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-01-22 11:56:36 -0300

    Project crud basic

---
+++ 22-crud/includes/mensagem.php
@@ -0,0 +1,16 @@
+<?php
+// SESSÃO
+session_start();
+if(isset($_SESSION['mensagem'])): ?>
+
+<script>
+  //Mensagem de sucesso
+  window.onload = function() {
+    M.toast({html: '<?php echo $_SESSION['mensagem']; ?>' })
+  };
+</script>
+
+<?php
+endif;
+session_unset();
+?>
---
+++ 22-crud/index.php
@@ -0,0 +1,81 @@
+<?php
+// Conexão
+include_once 'php_action/db_connect.php';
+
+// Header 
+include_once 'includes/header.php';
+// Message
+include_once 'includes/mensagem.php';
+?>
+
+<!-- INICIO TABELA -->
+<div class="row">
+    <div class="col s12 m6 push-m3">
+      <h3 class="light">Clientes</h3>
+      <table class="striped">
+        <thead>
+            <tr>
+              <th>Nome: </th>
+              <th>sobrenome: </th>
+              <th>Email: </th>
+              <th>Idade: </th>
+            </tr>
+        </thead>
+        <tbody>
+          <?php 
+          $sql = "SELECT * FROM clientes";  // Seleção de todos os dados do db
+          $resultado = mysqli_query($connect, $sql); 
+
+          if(mysqli_num_rows($resultado) > 0):
+
+          while($dados = mysqli_fetch_array($resultado)):
+          ?>
+            <tr>
+              <td><?php echo $dados['nome']; ?></td>
+              <td><?php echo $dados['sobrenome']; ?></td>
+              <td><?php echo $dados['email']; ?></td>
+              <td><?php echo $dados['idade']; ?></td>
+              <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
+              <td><a href="#modal<?php echo $dados['id']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
+            
+                              <!-- Modal Structure -->
+                <div id="modal<?php echo $dados['id']; ?>" class="modal">
+                  <div class="modal-content">
+                    <h4>Vish</h4>
+                    <p>Tem certeza que deseja excluir esse cliente?</p>
+                  </div>
+                  <div class="modal-footer">
+
+                    <form action="php_action/delete.php" method="POST">
+                      <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
+                      <button type="submit" name="btn-deletar" class="btn red">Confirmo a deleção</button>
+                      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
+                    </form>
+                  </div>
+                </div>
+                      
+            </tr>
+           <?php endwhile; 
+           else: ?>
+
+            <tr>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+            </tr>
+
+            <?php endif; ?>
+        </tbody>
+      </table>
+      <br>
+      <a href="adicionar.php" class="btn"> ADICIONAR CLIENTE</a>
+    </div>
+</div>
+<!-- FIM DA TABELA -->
+
+
+<?php
+// Footer
+include_once 'includes/footer.php'; 
+?>
